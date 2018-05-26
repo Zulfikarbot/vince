aa37abaaed5b9f7bb440ef0bcb9cc247f34548b7
YaroST12 <yaro330@gmail.com>
2018-05-27 01:10:30 +0300

    android: silence some logging
    Disable debug in LMK
    Signed-off-by: YaroST12 <yaro330@gmail.com>
    
    Change-Id: Ide5d7bc0f0e0eed4bc8cf8a74ae36051a496c65c
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Ahmad Thoriq Najahi <tlogitechnjhi@gmail.com>
    Signed-off-by: nancyjudge <botnancy1@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -41,7 +41,7 @@
 #include <linux/rcupdate.h>
 #include <linux/notifier.h>
 
-static uint32_t lowmem_debug_level = 1;
+static uint32_t lowmem_debug_level = 0;
 static short lowmem_adj[6] = {
 	0,
 	1,
