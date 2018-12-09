ef729620ba11bc2103efbabee074ec284ee8ee18
Rama Bondan Prakoso <ramanarubp@gmail.com>
2018-12-09 06:18:13 +0000

    drivers: zram: Import Xiaomi changes
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1157,7 +1157,8 @@ static void change_pageblock_range(struct page *pageblock_page,
  * If we claim more than half of the pageblock, change pageblock's migratetype
  * as well.
  */
-static bool can_steal_fallback(unsigned int order, int start_mt)
+static bool can_steal_fallback(unsigned int order, int start_mt,
+			int fallback_type, unsigned int start_order)
 {
 	/*
 	 * Leaving this order check is intended, although there is
@@ -1169,10 +1170,11 @@ static bool can_steal_fallback(unsigned int order, int start_mt)
 	if (order >= pageblock_order)
 		return true;
 
-	if (order >= pageblock_order / 2 ||
-		start_mt == MIGRATE_RECLAIMABLE ||
-		start_mt == MIGRATE_UNMOVABLE ||
-		page_group_by_mobility_disabled)
+	if ((start_mt != MIGRATE_UNMOVABLE && order >= pageblock_order / 2) ||
+	(start_mt == MIGRATE_UNMOVABLE && fallback_type != MIGRATE_MOVABLE && order >= pageblock_order / 2) ||
+	start_mt == MIGRATE_RECLAIMABLE ||
+	(start_mt == MIGRATE_UNMOVABLE && start_order >= 5) ||
+	page_group_by_mobility_disabled)
 		return true;
 
 	return false;
@@ -1207,7 +1209,7 @@ static void steal_suitable_fallback(struct zone *zone, struct page *page,
 
 /* Check whether there is a suitable fallback freepage with requested order. */
 static int find_suitable_fallback(struct free_area *area, unsigned int order,
-					int migratetype, bool *can_steal)
+					int migratetype, bool *can_steal, unsigned int start_order)
 {
 	int i;
 	int fallback_mt;
@@ -1224,7 +1226,7 @@ static int find_suitable_fallback(struct free_area *area, unsigned int order,
 		if (list_empty(&area->free_list[fallback_mt]))
 			continue;
 
-		if (can_steal_fallback(order, migratetype))
+		if (can_steal_fallback(order, migratetype, fallback_mt, start_order))
 			*can_steal = true;
 
 		return fallback_mt;
@@ -1249,7 +1251,7 @@ __rmqueue_fallback(struct zone *zone, unsigned int order, int start_migratetype)
 				--current_order) {
 		area = &(zone->free_area[current_order]);
 		fallback_mt = find_suitable_fallback(area, current_order,
-				start_migratetype, &can_steal);
+				start_migratetype, &can_steal, order);
 		if (fallback_mt == -1)
 			continue;
 
